6e695a651fb4b3b922d46037235bbe396aae196d
helamandc <manlehcastillo@gmail.com>
2025-11-06 01:21:42 +1100

    Added timeout and logout

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -14,6 +14,14 @@ test('Verify login', async ({ page }) => {
 
     await page.locator("button[type='submit']").click()
 
+    await page.waitForTimeout(5000)
+
     expect(page.url()).toContain('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index')
     //await expect(page).toHaveURL(/dashboard/)
+
+    await page.getByAltText('profile picture').click()
+
+    await page.getByText('Logout').click()
+
+    expect(page.url()).toContain('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
 })
